30540d057ff411079c575cf1b986e87e93ffdb57
Angie <el20110@mail.ntua.gr>
2025-02-10 19:15:35 +0200

    Create header.js

---
+++ front-end/static/header.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const homeLink = document.getElementById('home-link');
+    homeLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        window.location.href = 'dashboard'; // Κατευθύνει στη σελίδα Dashboard
+    });
+});
