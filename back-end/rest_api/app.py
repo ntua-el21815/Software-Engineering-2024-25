843d5a12818221032ea361e001d1b3b3eed21de4
ntua-el21815 <el21815@mail.ntua.gr>
2025-02-13 21:31:48 +0200

    ADMIN default creation

--- back-end/rest_api/app.py
+++ back-end/rest_api/app.py
@@ -3,6 +3,8 @@ from database.db_config import init_db
 from routes.admin_routes import admin_routes
 from routes.analysis_routes import analysis_routes
 from routes.auth_routes import auth_routes
+from models import TollOperator
+from database.db_config import db
 import os
 import socket
 
@@ -16,6 +18,19 @@ app.register_blueprint(admin_routes, url_prefix='/api/admin')
 app.register_blueprint(analysis_routes, url_prefix='/api')
 app.register_blueprint(auth_routes, url_prefix='/api')
 
+# Συνάρτηση που εγγυάται πως θα υπάρχει ADMIN user στη βάση δεδομένων
+def create_admin():
+    admin_exists = db.session.query(TollOperator).filter_by(OpID='admin').first()
+    if admin_exists is None:
+        admin_user = TollOperator(
+            OpID='ADMIN',
+            Name='administrator',
+            Email='admin@example.com',
+            password='freepasses4all'  
+        )
+        db.session.add(admin_user)
+        db.session.commit()
+
 # Ρίζα για δοκιμή
 @app.route('/')
 def index():
@@ -27,5 +42,14 @@ if __name__ == '__main__':
     # Τοποθεσία του αρχείου του SSL certificate
     context = (this_dir + '/ssl/server.crt', this_dir + '/ssl/server.key')
     # Απόκτηση της ip του server
-    ip = socket.gethostbyname(socket.gethostname())
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('8.8.8.8', 80))
+        ip = s.getsockname()[0]
+    finally:
+        s.close()
+    with app.app_context():
+        # Δημιουργία του admin user
+        create_admin()
+    # Εκκίνηση του server
     app.run(host=ip, port=9115, ssl_context=context, debug=True)
